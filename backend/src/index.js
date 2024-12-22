f688db5da09b6c164f25e264dcaed461505e8950
Rohit <dv6510@gmail.com>
2024-12-22 22:05:02 +0530

    done with few stuffs

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import connectDB from './db/db.js';
+import { app } from './app.js';
+
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((error) => {
+    console.error("MongoDB connection failed !!!", error);
+});
